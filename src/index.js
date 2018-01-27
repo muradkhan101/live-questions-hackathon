6e574023207952bbc3e84d2cba9942cbc3b0000c
evgimov <evgimov@gmail.com>
2018-01-27 14:18:54 -0800

    Added routes,attendee and presenter components

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import Main from './react/main';
+import Attendee from './react/attendee';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path='/presenter' component={presenter} />
+        <Route path='/attendee' component={Attendee} />
+   </Router>), document.getElementById('root'));
 registerServiceWorker();
---
+++ src/react/presenter.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class Presenter extends React.Component {
+    render() {
+        <div>
+            <h1>Hello from presenter component </h1>
+        </div>
+    }
+}
