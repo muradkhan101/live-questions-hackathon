c5aeb0392c7a0d1397f236a64b5ecf7091f87237
evgimov <evgimov@gmail.com>
2018-02-10 11:12:28 -0800

    Rendering HomeComponent instead of the routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import {
-    HashRouter,
-    Route
-  } from 'react-router-dom';
+import HomeComponent from './react/home';
 import registerServiceWorker from './registerServiceWorker';
-import Attendee from './react/attendee';
-import Presenter from './react/presenter';
 
 ReactDOM.render((
-    <HashRouter>
-        <div>
-            <Route path='/presenter' component={Presenter} />
-            <Route path='/attendee' component={Attendee} />
-        </div>
-   </HashRouter>), document.getElementById('root'));
+   <HomeComponent/> ), document.getElementById('root'));
 registerServiceWorker();
